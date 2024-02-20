ad4863f6f9114a2dfbe25dc054532a447c3d23ad
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2024-02-20 08:59:25 -0800

    opening a repository

---
+++ src/views/buy.js
@@ -0,0 +1,19 @@
+// folder views - file buy
+
+import React from 'react';
+import Header from '../components/header';
+import SearchInput from '../components/SearchInput.js';
+import Footer from "../components/footer"
+
+function Buy() {
+    return (
+        <div>
+            <Header />
+            <SearchInput />
+            <Footer/>
+            
+        </div>
+    );
+}
+
+export default Buy;
---
+++ src/views/sell.js
@@ -0,0 +1,19 @@
+// folder views - file sell
+import React from 'react'
+import Header from '../components/header'
+import SellForm from '../components/sellForm';
+import Footer from "../components/footer"
+
+
+function Sell() {
+    return (
+        <div>
+            <Header/>
+            <SellForm/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default Sell;
+//
